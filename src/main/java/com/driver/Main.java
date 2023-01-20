e23fbc4720e0cf9eaac2de3976ebdb507292f8c5
vipin <vipinkmundra@gmail.com>
2023-01-20 20:31:50 +0530

    solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,7 +9,7 @@ public class Main {
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(false);
+    Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
