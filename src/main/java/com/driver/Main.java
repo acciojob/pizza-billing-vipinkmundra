0f4c3d8f53594281b792992941aa7074ab15e46a
vipin <vipinkmundra@gmail.com>
2023-01-20 22:11:59 +0530

    solution3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,10 +9,10 @@ public class Main {
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
+    Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-    p.addTakeaway();
+//    p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,58 +6,74 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int cheesePrice;
+    private int toppingPrice;
+    boolean isCheeseAdded;
+    boolean isToppingAdded;
+    boolean isTakeAwayAdded;
+    boolean isBillGenerated;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        this.isCheeseAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeAwayAdded = false;
+        this.isBillGenerated = false;
+        this.bill = "";
         if(isVeg == true){
             this.price = 300;
+            this.toppingPrice = 70;
         }else{
             this.price = 400;
+            this.toppingPrice = 120;
         }
-        // your code goes here
+        this.cheesePrice = 80;
+        this.bill = this.bill + "Base Price Of The Pizza: " + this.price;
     }
 
     public int getPrice(){
         return this.price;
     }
 
-    boolean flag1 = false;
+//    boolean flag1 = false;
     public void addExtraCheese(){
         // your code goes here
-        if(flag1 == false){
-            this.price+=80;
-            flag1 = true;
+        if(isCheeseAdded == false){
+            this.price+=cheesePrice;
+            isCheeseAdded = true;
         }
     }
 
-    boolean flag2 = false;
     public void addExtraToppings(){
         // your code goes here
-        if(flag2 == false){
-            if(isVeg == true){
-                this.price+=70;
-            }else{
-                this.price+=120;
-            }
-            flag2 = true;
+        if(isToppingAdded == false){
+                this.price+=toppingPrice;
+            isToppingAdded = true;
         }
     }
 
-    boolean flag3 = false;
     public void addTakeaway(){
         // your code goes here
-        if(flag3 == false){
+        if(isTakeAwayAdded == false){
             this.price+=20;
-            flag3 = true;
+            isTakeAwayAdded = true;
         }
     }
 
     public String getBill(){
         // your code goes here
-        this.bill = ("Base Price Of The Pizza: " + (isVeg?300:400)+
-                (flag1?"\nExtra Cheese Added: 80":" ")+
-                (flag2?"\nExtra Toppings Added: "+(isVeg?70:120):" ")+
-                (flag3?"\nPaperbag Added: 20":" ")+
-                "\nTotal Price: "+this.price);
+        if(isBillGenerated == false){
+            if(isCheeseAdded){
+                this.bill+="\nExtra Cheese Added: "+cheesePrice;
+            }
+            if(isToppingAdded){
+                this.bill+="\nExtra Toppings Added: "+toppingPrice;
+            }
+            if(isTakeAwayAdded){
+                this.bill+="\nExtra Toppings Added: "+20;
+            }
+            this.bill+="\nTotal Price: "+this.price+"\n";
+            isBillGenerated = true;
+        }
         return this.bill;
     }
 }
